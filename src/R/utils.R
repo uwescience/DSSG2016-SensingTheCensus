b1af7dcce15217958f4a31e8bf9d11d50a9b8288
Carlos Espino <carlos.espino@columbia.edu>
2016-06-22 11:51:41 -0700

    Add R script to read census data and create a deprivation index

---
+++ src/R/utils.R
@@ -0,0 +1,26 @@
+multmerge = function(mypath, pattern, read_function){
+  filenames = list.files(path=mypath, full.names=TRUE)
+  filenames = filenames[grepl(pattern,filenames)]
+  datalist = lapply(filenames, function(x){
+    read_function(x)
+  })
+  # datalist
+  Reduce(function(x,y) {bind_rows(x,y)}, datalist) %>% tbl_df()
+}
+
+read_census_data = function(file_path) {
+  read_delim(file=file_path, delim = ";", na = "null", col_types = cols(
+    "CODREG" = col_character(),
+    "REGIONE" = col_character(),
+    "CODPRO" = col_character(),
+    "PROVINCIA" = col_character(),
+    "CODCOM" = col_character(),
+    "COMUNE" = col_character(),
+    "PROCOM" = col_character(),
+    "SEZ2011" = col_character(),
+    "NSEZ" = col_character(),
+    "ACE" = col_character(),
+    "CODLOC" = col_character(),
+    "CODASC"= col_character()
+  ))
+}
