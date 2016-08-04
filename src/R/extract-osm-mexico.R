a0fd6e0edcc486385c3b7221ceb748230f697646
Rachael Dottle <rachaelcdottle@gmail.com>
2016-08-03 17:06:11 -0700

    Edit script to fix offering advantage calculation

--- src/R/extract-osm-mexico.R
+++ src/R/extract-osm-mexico.R
@@ -48,18 +48,18 @@ proj4string(amenities) = mexicoproj
 intersection = raster::intersect(y = census, x = amenities)
 
 #intersect amenity and census data
-count_cat = intersection@data %>% group_by(CVE_GEOAGE, amenity) %>% summarize(count_cat = n()) 
-count_geo = count_cat %>% group_by(CVE_GEOAGE) %>% summarize(count_geo = n()) 
+count_cat = intersection@data %>% group_by(CVE_GEOAGE, amenity) %>% dplyr::summarize(count_cat = n()) 
+count_geo = count_cat %>% group_by(CVE_GEOAGE) %>% dplyr::summarize(count_geo = n()) 
 total_poi = dim(intersection@data)[1]
 #intersection@data[1]
-total_poi_cat = intersection@data %>% group_by(amenity) %>% summarize(poi_cat = n()) 
+total_poi_cat = intersection@data %>% group_by(amenity) %>% dplyr::summarize(poi_cat = n()) 
 
 #calculate offering advantage 
 offer_advantage = count_cat %>% 
 left_join(count_geo, by ="CVE_GEOAGE") %>% 
   left_join(total_poi_cat, by ="amenity") %>%
   mutate(total_poi = total_poi) %>%
-  mutate(offer_advantage = (count_cat/count_geo)*(poi_cat/total_poi))
+  mutate(offer_advantage = (count_cat/count_geo)*(total_poi/poi_cat))
 offer_advantage_wide = offer_advantage%>% dplyr::select(CVE_GEOAGE, amenity, offer_advantage) %>% spread(amenity, offer_advantage, fill = 0)
 
 #join census and amenity data 
@@ -79,7 +79,7 @@ osm_pca_df = as.data.frame(osm_pca$x[,1:36])
 props <- variance / sum(variance)
 cum_variance = cumsum(props)
 biplot(osm_pca)
-
+summary(osm_pca)
 #create csv of amenity, pca, and deprivation
 census@data %<>% bind_cols(osm_pca_df)
 census@data %>% dplyr::select(CVE_GEOAGE, IMU, starts_with("PC"), arts_centre:waste) %>% 
