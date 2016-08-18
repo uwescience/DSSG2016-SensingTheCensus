95dbeeac0526326efe6a026435b6261bdf6f5036
Carlos Espino <carlos.espino@columbia.edu>
2016-08-17 18:57:16 -0700

    Add styles and js

---
+++ census-app/sync_maps.js
@@ -0,0 +1,16 @@
+(window.onload = function() {
+    
+    setTimeout(function () {
+    var leaf_widgets = Array.prototype.map.call(
+
+      document.querySelectorAll(".leaflet"),
+      function(ldiv){
+        return HTMLWidgets.find("#" + ldiv.id);
+      }
+    );
+
+    // make this easy since we know only two maps
+    leaf_widgets[0].sync(leaf_widgets[1]);
+    leaf_widgets[1].sync(leaf_widgets[0]);  
+}, 5000);
+})();
