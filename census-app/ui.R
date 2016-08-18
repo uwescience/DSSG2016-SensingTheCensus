151c366ad28a58d78a02a686ad72972f00917b3c
Carlos Espino <carlos.espino@columbia.edu>
2016-08-17 19:11:45 -0700

    Add missing lines

--- census-app/ui.R
+++ census-app/ui.R
@@ -11,6 +11,15 @@ library(tidyr)
 library(RColorBrewer)
 
 
+milan_streets = readRDS("milan_street_intersection.rds")
+mexico_streets = readRDS("mexico_city_street_intersection.rds")
+
+milan_census = readRDS("milan.rds") 
+mexico_census = readRDS("mexico_city.rds")
+
+milan_amenities = readRDS("milan_amenities.rds")
+mexico_amenities = readRDS("mexico_city_amenities.rds")
+
 poly_ids = list("milan" = "ACE", "mexico_city" = "CVE_GEOAGE")
 census = list("milan" = milan_census, "mexico_city" = mexico_census)
 streets =  list("milan" = milan_streets, "mexico_city" = mexico_streets )
